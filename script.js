a8583dc0b6783741de4aa93c55af64431d785b8c
Tar Yar Win Htet <120553433+LimeAlz@users.noreply.github.com>
2023-11-04 15:00:18 +1030

    Finsihed

--- script.js
+++ script.js
@@ -19,27 +19,41 @@ function Book(name,author,pages,haveRead) {
   
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary(book,i) {
   // do stuff here
   const newRow = document.createElement('tr');
   const nameData = document.createElement('td');
+  nameData.setAttribute("data-number",i)
   nameData.textContent = book.name;
 
   const authorData = document.createElement('td');
+  authorData.setAttribute("data-number",i)
   authorData.textContent = book.author;
 
   const pageData = document.createElement('td');
+  pageData.setAttribute("data-number",i)
   pageData.textContent = book.pages;
 
   const haveReadData = document.createElement('td');
+  haveReadData.setAttribute("data-number",i)
   haveReadData.classList.add('read');
   haveReadData.textContent = book.haveRead;
 
   const haveFinished = document.createElement('td');
+  haveFinished.setAttribute("data-number",i)
   const finished =  document.createElement('button');
+  finished.setAttribute("data-number",i)
   finished.textContent = "Finished";
   finished.classList.add("finished")
   haveFinished.appendChild(finished);
+
+  const RemoveButton = document.createElement('td');
+  RemoveButton.setAttribute("data-number",i)
+  const removed =  document.createElement('button');
+  removed.setAttribute("data-number",i)
+  removed.textContent = "Remove Item";
+  removed.classList.add("removed")
+  RemoveButton.appendChild(removed);
 
 
   newRow.appendChild(nameData);
@@ -47,6 +61,7 @@ function addBookToLibrary(book) {
   newRow.appendChild(pageData);
   newRow.appendChild(haveReadData);
   newRow.appendChild(haveFinished);
+  newRow.appendChild(RemoveButton);
 
   table.appendChild(newRow);
 }
@@ -59,7 +74,7 @@ showbutton.addEventListener('click',
 
 
 for (let i=0; i< myLibrary.length ;i++){
-  addBookToLibrary(myLibrary[i]);
+  addBookToLibrary(myLibrary[i],i);
 }
 
 button.addEventListener('click', (e)=>{
@@ -71,24 +86,12 @@ button.addEventListener('click', (e)=>{
   }
   let tempbook = new Book(formName.value, formAuthor.value, formPages.value,tempo)
   myLibrary.push(tempbook);
-  addBookToLibrary(tempbook);
+  addBookToLibrary(tempbook,myLibrary.findIndex(x=>x == tempbook));
   console.log(myLibrary);
   formName.value = "", formAuthor.value = "" , formPages.value = "", formHaveRead.value="";
 }
   
 )
-
-// // const finishButton = document.querySelector('.finished');
-// finished.addEventListener('click',(e)=>{
-//     e.preventDefault();
-//     if (haveReadData.textContent === 'true'){
-//       haveReadData.textContent  = false;
-//     }
-//     else{
-//       haveReadData.textContent  = true;
-//     }
-// }
-// )
 
 table.addEventListener('click', (e) => {
   if (e.target.classList.contains('finished')) {
@@ -104,3 +107,25 @@ table.addEventListener('click', (e) => {
     }
   }
 });
+
+
+const RemoveButton = document.querySelector(".removed");
+
+
+table.addEventListener('click',(e)=>{
+  if (e.target.classList.contains('removed')) {
+    e.preventDefault();
+    const clickedButton = e.target;
+  
+  const number = clickedButton.dataset.number;
+  const elements = document.querySelectorAll(`[data-number='${number}']`);
+
+  elements.forEach(element => {
+    element.remove();
+  });
+  myLibrary.splice(number,number);
+  console.log(myLibrary);
+
+
+  }
+})
